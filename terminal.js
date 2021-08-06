2075087f94ace02121e43a2f4b7fd81a7bf7d376
ilmar <ilmar.tyrk@gmail.com>
2021-08-06 16:12:32 +0300

    Update code

--- terminal.js
+++ terminal.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var _ = require('lodash');
-var htmlmd = require('html-md-2');
-var marked = require('marked');
-var qi = require('q-i')
-var TerminalRenderer = require('marked-terminal');
-var options = {
+const htmlmd = require('html-md-2');
+const marked = require('marked');
+const qi = require('q-i')
+const TerminalRenderer = require('marked-terminal');
+marked.setOptions = {
   renderer: new TerminalRenderer()
 };
 
@@ -13,10 +12,10 @@ module.exports = function terminal (modelMapper) {
   return {
     name: 'terminal',
     send: function (model, done) {
-      var md = htmlmd(model.body);
-      var term = marked(md, options);
+      const md = htmlmd(model.body);
+      const term = marked(md);
 
-      var modelBits = modelMapper ? modelMapper(model) : model
+      const modelBits = modelMapper ? modelMapper(model) : model
 
       console.log(qi.stringify(modelBits));
       console.log(term);
